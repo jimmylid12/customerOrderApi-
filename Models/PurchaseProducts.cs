00caa9a5d53c4793fc7bcf2975ba4b9fd8465fb9
jimmylid12 <jameslid12@msn.com>
2019-12-10 15:52:45 +0000

    new commit

---
+++ Models/PurchaseProducts.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+namespace CustomersOrderService.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string ProductName{ get; set; }
+        public string price{ get; set; }
+        public string quantity { get; set; }
+    }
+}
